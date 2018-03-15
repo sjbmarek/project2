2678f1faa9f8bfc3cc7be0e3ab676ccfd7987c0a
Susan Marek <smarek@chaskamn.com>
2018-03-14 21:33:12 -0500

    file set up more

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,7 @@ USE dogs;
 
 INSERT INTO dogs (dog_name, owner_name, dog_comment, dog_photo) 
 VALUES ('Whitney', 'Susan Marek', 'Whitney loves the dog park!', "../public/assets/img/Whitney.jpeg");
+
+-- MySQL will not take a path to upload a file, it must be handled by fs
+
+-- Good example here:  http://www.technicalkeeda.com/nodejs-tutorials/nodejs-store-image-into-mysql-database
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them. UPDATE THIS
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/dog_controller.js");
 
 app.use(routes);
 
